2bad391fce06ff092e738dfdd4899d18c6482ce7
Evan Battaglia <ebattaglia@instructure.com>
2023-07-27 22:17:51 +0000

    minor cleanup

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,27 @@ mod machine;
 use anyhow::Context;
 use types::TabryConf;
 
-// #[derive(Debug, thiserror::Error)]
-// enum TabryConfigError {
-//     #[error("Could not read tabry config: {0}")]
-//     IoError(#[from] std::io::Error),
-//     #[error("Tabry config bad JSON: {0}")]
-//     JsonError(#[from] serde_json::Error),
-// }
-
 fn config_from_file(filename: &str) -> anyhow::Result<TabryConf> {
-    let conf_str = std::fs::read_to_string(filename)?;
-    let conf: TabryConf = serde_json::from_str(&conf_str)?;
+    let conf_str = std::fs::read_to_string(filename).
+        with_context(|| "reading file failed")?;
+    let conf: TabryConf = serde_json::from_str(&conf_str).
+        with_context(|| "parsing file failed")?;
     Ok(conf)
 }
 
 fn run() -> anyhow::Result<()> {
-    let mut args_iter = std::env::args().into_iter();
-    let config_file = args_iter.next().with_context(|| "missing argument context_file")?;
-    let mut machine = machine::Machine::new(config_from_file(&config_file)?);
-    for arg in args_iter {
+    let args = std::env::args().collect::<Vec<String>>();
+
+    let config_file = args.get(1).with_context(|| "missing argument context_file")?;
+    let config = config_from_file(&config_file).with_context(|| "invalid config file")?;
+
+    let mut machine = machine::Machine::new(config);
+    for arg in &args[2..] {
         // TODO: use std::error::Error trait in Machine so can use "?" here instead of unwrap()
         machine.next(&arg).unwrap();
     }
+
+    println!("{}", serde_json::to_string_pretty(&machine.state)?);
     Ok(())
 }
 
