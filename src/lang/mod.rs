02d2efb1cdd56b39a1d51efd50383e13a4679fca
Evan Battaglia <ebattaglia@instructure.com>
2024-09-05 14:34:34 -0230

    Tests from old tabry
    Some are not passing, put in 'TODO' directory for now

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -28,3 +28,25 @@ pub fn compile(tabry_file_str: &str) -> Result<crate::core::config::TabryConf, L
 
     Ok(res)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_helpers::*;
+    use assert_json_diff::assert_json_eq;
+
+    #[test]
+    fn test_integration_examples_from_language_reference() {
+        for example in
+            each_file_in_dir_with_extension("fixtures/examples_from_language_reference", "tabry")
+        {
+            let tabry_file_str = load_fixture_file_text(&example);
+            let res = compile(&tabry_file_str).unwrap();
+            let expected = load_fixture_file::<crate::core::config::TabryConf>(
+                example.replace(".tabry", ".json").as_str(),
+            );
+            eprintln!("checking example {example}");
+            assert_json_eq!(res, expected);
+        }
+    }
+}
--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -1,7 +1,42 @@
 use serde::Deserialize;
+use std::borrow::Cow;
 use std::fs;
 
+pub fn load_fixture_file_text(filename: &str) -> String {
+    let path: Cow<str> = if filename.starts_with("fixtures/") {
+        Cow::Borrowed(filename)
+    } else {
+        Cow::Owned(format!("fixtures/{filename}"))
+    };
+    fs::read_to_string(path.as_ref())
+        .unwrap_or_else(|_| panic!("Failed to read fixtures/{filename}"))
+}
+
+pub fn each_file_in_dir_with_extension<'a>(
+    dir: &'a str,
+    extension: &'a str,
+) -> impl Iterator<Item = String> + 'a {
+    let dir_iter = fs::read_dir(dir).unwrap_or_else(|_| panic!("Failed to read dir {dir}"));
+    dir_iter
+        .filter(move |entry| {
+            let entry = entry
+                .as_ref()
+                .unwrap_or_else(|_| panic!("Error with DirEntry {entry:?}"));
+            match entry.path().extension() {
+                Some(ext) => {
+                    let actual_ext = ext
+                        .to_str()
+                        .unwrap_or_else(|| panic!("Bad UTF-8 {entry:?}"));
+                    actual_ext == extension
+                }
+                None => false,
+            }
+        })
+        .map(|entry| entry.unwrap().path().to_str().unwrap().to_string())
+}
+
 pub fn load_fixture_file<T: for<'a> Deserialize<'a>>(filename: &str) -> T {
-    let file_str = fs::read_to_string(format!("fixtures/{filename}")).unwrap();
-    serde_json::from_str::<T>(&file_str).unwrap()
+    let file_str = load_fixture_file_text(filename);
+    serde_json::from_str::<T>(&file_str)
+        .unwrap_or_else(|_| panic!("Failed to parse fixtures/{filename}"))
 }
