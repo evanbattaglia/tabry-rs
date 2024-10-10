577ce13ce41f01927e5b3ba2d1a0ce1fbfb6c768
Evan Battaglia <ebattaglia@instructure.com>
2024-10-10 20:17:12 +0000

    version bump

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.rustPlatform.buildRustPackage {
   pname = "tabry";
-  version = "0.0.1";
+  version = "0.1.0";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
 }
